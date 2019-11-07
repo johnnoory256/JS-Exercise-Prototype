a6a43f9721e1e08f9937173bd20ca4bd08ac0b2a
johnnoory256 <noory.bs2016@gmail.com>
2019-11-07 07:27:22 -0800

    Task 4 four principles of this keyword is done

--- index.js
+++ index.js
@@ -125,18 +125,26 @@ Car.prototype.toString = function (){
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age,favoriteToy) {
+  Person.call(this, favoriteToy);
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
 }
 
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+  return `playing with ${this.favoriteToy}!`;
+}
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Default binding
+  2. implicit binding
+  3. expcilit binding
+  4. new binding
 */
 
 
